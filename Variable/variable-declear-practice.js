cf885c046cbd1fcf14683b2f70249847eeb230fd
hridayshadhukha <hridayshadhukha@gmail.com>
2025-03-29 09:51:58 +0600

    first commit

---
+++ Variable/variable-declear-practice.js
@@ -0,0 +1,23 @@
+// practice - 1
+const birthYear = 2003;
+
+// practice - 2
+const brotherSister = "1 brother and 1 sister";
+
+// practice- 3
+const students = 114;
+
+// practice- 4
+const marks = 33;
+
+// practice- 5
+const rooms = 2;
+
+// practice- 6
+const readingHours = 6;
+
+// practice- 7
+const sum = 0 + 1 + 3 + 1 + 4 + 8 + 0 + 7 + 5 + 8 + 9;
+const total = sum;
+
+
